d068ef72b11e2679bd46b6f28b515d5c459270c4
Inoi-K <greatoat@gmail.com>
2022-11-17 23:51:01 +0300

    similarity sorting

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,5 +1,7 @@
 package utils
 
+import "sort"
+
 // intersect gets two sets and returns their intersection
 func intersect(s1, s2 map[string]struct{}) map[string]struct{} {
 	s3 := make(map[string]struct{})
@@ -28,4 +30,24 @@ func JaccardIndex(s1, s2 map[string]struct{}) float64 {
 	intersection := intersect(s1, s2)
 
 	return float64(len(intersection)) / float64(len(s1)+len(s2)-len(intersection))
+}
+
+type KeyValue struct {
+	Key   string
+	Value float64
+}
+
+func SortSetByValue(m map[string]float64) []KeyValue {
+	s := make([]KeyValue, len(m))
+	i := 0
+	for k, v := range m {
+		s[i] = KeyValue{Key: k, Value: v}
+		i++
+	}
+
+	sort.Slice(s, func(i int, j int) bool {
+		return s[i].Value > s[j].Value
+	})
+
+	return s
 }
